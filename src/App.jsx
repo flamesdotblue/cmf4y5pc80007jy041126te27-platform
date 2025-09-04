79fddd489961b4e6fb4fc483fc7e3831e476ec54
Flames <flames@flames.blue>
2025-09-04 06:14:35 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import Hero from './components/Hero';
 import Steps from './components/Steps';
-import BentoGallery from './components/BentoGallery';
 import StepsCompact from './components/StepsCompact';
+import PartnersMarquee from './components/PartnersMarquee';
+import Mentors from './components/Mentors';
+import WallOfLove from './components/WallOfLove';
+import BentoGallery from './components/BentoGallery';
+import FAQ from './components/FAQ';
+import StatsCTA from './components/StatsCTA';
 
 export default function App() {
   return (
@@ -10,7 +15,12 @@ export default function App() {
       <Hero />
       <Steps />
       <StepsCompact />
+      <PartnersMarquee />
+      <Mentors />
+      <WallOfLove />
       <BentoGallery />
+      <FAQ />
+      <StatsCTA />
       <footer className="py-10 text-center text-sm text-white/60">
         © {new Date().getFullYear()} AlgoUniversity · Hiring Tournament
       </footer>
